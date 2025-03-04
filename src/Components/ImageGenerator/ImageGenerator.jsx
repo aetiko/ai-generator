9fc7c7ab12659721405ad50955ff5feebc5949db
ayo <nurudeenetiko12@gmail.com>
2025-03-04 14:57:35 -0700

    creating loading bar under image

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -5,12 +5,14 @@ import default_image from "../Assets/4.png";
 const ImageGenerator = () => {
   const [image_url, setImage_url] = useState("/");
   let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
   const imageGenerator = async () => {
     if (inputRef.current.value === "") {
       return 0;
     }
+    setLoading(true);
     const response = await fetch(
       "https://api.openai.com/v1/images/generations",
       {
@@ -32,6 +34,7 @@ const ImageGenerator = () => {
     console.log("API Response:", data);
     let data_array = data.data;
     setImage_url(data_array[0].url);
+    setLoading(false);
   };
   return (
     <div className="ai_image_generator">
@@ -40,7 +43,10 @@ const ImageGenerator = () => {
       </div>
       <div className="img_loading">
         <div className="image">
-          <img src={image_url === "/" ? default_image : image_url} alt="" />
+          <img src={image_url === "/" ? default_image : image_url} alt="" /></div>
+          <div className="loading">
+            <div className={loading?"loading_bar_full":"loading_bar"}></div>
+              <div className={loading?"loading_text":"display_none"}>Loading...</div>
         </div>
       </div>
       <div className="search_box">
